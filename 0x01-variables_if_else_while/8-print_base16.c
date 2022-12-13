e7edc32af8e920737e200a411ca1e208dabda677
MetyEstalu <Metyswety@yahoo.com>
2022-12-13 01:35:05 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,13 @@ int main(void)
 {
 char ch;
 for (ch = '0'; ch <= '9'; ch++)
-	{
+{
 	putchar(ch);
-	}	
+}	
 	for (ch = 'a'; ch <= 'f'; ch++)
-		{
-			putchar(ch);
-		}
+	{
+		putchar(ch);
+	}
 putchar('\n');
 return (0);
 }
