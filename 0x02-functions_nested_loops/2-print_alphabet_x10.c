fe31789d72acf35375131beb4ef4dab1a3dc2636
MetyEstalu <metyswety@yahoo.com>
2023-05-24 03:47:52 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,18 @@
 #include "main.h"
-/**
- * mul - returns the product of its parameters
- * @a: int type number
- * @b: int type number
- * Return:0
- */
 
-int mul(int a, int b)
+/**
+* print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+*/
+void print_alphabet_x10(void)
 {
-	return (a * b);
+	char i, l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
 }
